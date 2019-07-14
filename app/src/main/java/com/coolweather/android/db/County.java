db440824bf6feaf3381ba130dc1d135b222d191b
Pluto1210 <1264317437@qq.com>
2019-07-14 13:53:16 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/coolweather/android/db/County.java
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -4,7 +4,7 @@ package com.coolweather.android.db;
 
 import org.litepal.crud.DataSupport;
 
-public class Country extends DataSupport {
+public class County extends DataSupport {
     private  int id;
     private String countyName;
     private   String weatherId;
